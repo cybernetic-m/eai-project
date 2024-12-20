2c1e4697a1fd8425a66549a570a5523d2b9d943c
Massimo Romano <romano.massimo.uni@gmail.com>
2024-12-20 14:11:49 +0100

    training functions

---
+++ training/train.py
@@ -0,0 +1,51 @@
+import os
+import sys
+# Get the absolute paths of the directories containing the utils functions and train_one_epoch
+training_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../training'))
+# Add these directories to sys.path
+sys.path.append(training_path)
+
+from train_one_epoch import train_one_epoch
+
+import numpy as np
+import torch
+
+def train(num_epochs, loss_fn, model, optimizer, training_dataloader, validation_dataloader): 
+
+    best_vloss = 1000000000
+
+    for epoch in range(num_epochs):
+        print(f'EPOCH {epoch + 1}/{num_epochs}:')
+
+        # Compute the loss_avg, y_pred and y_true
+        loss_avg, y_true_list, y_pred_list = train_one_epoch(model, optimizer, loss_fn, training_dataloader, train=True)
+
+        # Calculate the Root Mean Square Error metrics
+        train_se = 0 # Initialize the Mean Square Error
+ 
+        train_list_len = len(y_pred_list)
+        n_ts = train_list_len * len(y_pred_list[0])
+
+        for i in range(train_list_len):
+            y_pred = np.array(y_pred_list[i])
+            y_true = np.array(y_true_list[i])
+            train_se += np.sum((y_pred-y_true)**2)
+        train_rmse = np.sqrt(train_se / n_ts)
+
+        # Validation 
+        with torch.no_grad():
+            vloss_avg, vy_true_list, vy_pred_list = train_one_epoch(model, optimizer, loss_fn, validation_dataloader, train=False)
+        
+        # Calculate the Root Mean Square Error metrics
+        train_se = 0 # Initialize the Mean Square Error
+        
+        valid_list_len = len(vy_pred_list)
+        n_vs = valid_list_len * len(vy_pred_list[0])
+
+        for i in range(valid_list_len):
+            vy_pred = np.array(vy_pred_list[i])
+            vy_true = np.array(vy_true_list[i])
+            valid_se += np.sum((vy_pred-vy_true)**2)
+        valid_rmse = np.sqrt(valid_se / n_vs)
+        
+        print("Epoch %d: train LOSS %.4f valid LOSS %.4f train RMSE %.4f valid RMSE %.4f" % (epoch, loss_avg, vloss_avg, train_rmse, valid_rmse))
---
+++ training/train_one_epoch.py
@@ -0,0 +1,56 @@
+import numpy as np
+import gc
+import torch
+
+def train_one_epoch(model, optimizer, loss_fn, dataloader, train):
+
+    # Initialize the lists of predictions and true values
+    y_pred_list = []
+    y_true_list = []
+
+    loss_epoch = 0 # Initialize the loss
+    if train:
+        model.train() # Set the model in Training mode
+    else:
+        model.eval() # Set the model in Evaluation mode
+    
+    for X, y_true in dataloader:
+        
+        # Set the device
+        device = X.device
+
+        if train:
+            # Put the gradient to zero
+            optimizer.zero_grad()
+        
+        # Make predictions 
+        y_pred = model(X)
+        #print(y_pred)
+
+        # Compute the loss
+        loss = loss_fn(y_pred, y_true)
+
+        if train:
+            # Compute the gradient
+            loss.backward()
+
+            # Update the weights
+            optimizer.step()
+        
+        # Accumulate the loss in this epoch
+        loss_epoch += loss.detach().item()
+
+        # Add predictions and true values to the lists
+        y_true_list += y_true.cpu().tolist()
+        y_pred_list += y_pred.cpu().tolist()
+
+        del X, y_true, y_pred, loss
+        gc.collect()
+
+    if device == 'cuda':
+        torch.cuda.empty_cache()
+        
+    # Compute the average loss
+    loss_avg = loss_epoch / len(dataloader)
+
+    return loss_avg, y_true_list, y_pred_list

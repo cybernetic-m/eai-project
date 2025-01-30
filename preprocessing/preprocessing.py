75ffc718226a1082073339bb7fc2e47095d70d8c
Massimo Romano <romano.massimo.uni@gmail.com>
2025-01-30 18:16:39 +0100

    gradient better computation (denoised)

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import torch
+import numpy as np
+from numpy.polynomial.polynomial import Polynomial as Poly
+
 
 def normalize_columns(df: pd.DataFrame, columns: list, new_min: float, new_max: float) -> pd.DataFrame:
     """
@@ -113,4 +116,39 @@ def load_dataset_dict_pandas(csv_path):
         test = pd.read_csv(csv_path + '/' + filename)
         print(filename)
         print(test.info())
+
+def my_gradient(dataset, window_size):
+    
+    # Instantiate the three lines to compute the gradient (estimated as the angular coefficient of the lines)
+    x_line = Poly(coef=[0,1])
+    y_line = Poly(coef=[0,1])
+    z_line = Poly(coef=[0,1])
+
+    # Create the lists for all the gradients (len(dataset) - window_size gradients)
+    gradients = []
+
+    samples = [] # List of 100 temporary samples
+    for i in range(window_size, len(dataset)):
+        samples = dataset[i-window_size:i] # Take 100 elements of X1, Y1, Z1 
+        
+        # Take all rows of the first columns X1, Y1, Z1
+        x1_data = samples[:,0] 
+        y1_data = samples[:,1] 
+        z1_data = samples[:,2] 
+        
+        # Fit a line to 100 points of X1, Y1, Z1
+        x1_line = x_line.fit(x=range(100), y=x1_data, deg=1) 
+        y1_line = y_line.fit(x=range(100), y=y1_data, deg=1) 
+        z1_line = z_line.fit(x=range(100), y=z1_data, deg=1) 
+        
+        # Return the coefficients in form [q,m] and take the angular coefficients
+        x1_m = x1_line.convert().coef[1] 
+        y1_m = y1_line.convert().coef[1] 
+        z1_m = z1_line.convert().coef[1]
+
+        # Append the value of the gradients
+        gradients.append([x1_m, y1_m, z1_m])
+
+    return np.array(gradients) 
+
         

75becfb3b56ed0d9ba4cf11e92d1c730f537f5bd
Massimo Romano <romano.massimo.uni@gmail.com>
2025-02-20 11:10:28 +0100

    training implementation for autoencoder

--- training/train.py
+++ training/train.py
@@ -14,38 +14,65 @@ from calculate_metrics import calculate_metrics # type: ignore
 import numpy as np
 import torch
 
-def train(num_epochs, loss_fn, model, optimizer, scheduler, training_dataloader, validation_dataloader, hyperparams, model_dict, complete): 
+def train(num_epochs, loss_fn, model, optimizer, scheduler, training_dataloader, validation_dataloader, hyperparams, model_dict, autoencoder_dict, complete): 
 
     best_vloss = 1000000000
 
     # Definition of the dictionary of metrics
-    train_metrics = {
+    train_model_metrics = {
         'rmse':[], 
         'mae':[],
         'r2':[],
         'loss':[]       
                }
-    valid_metrics = {
+    train_autoencoder_metrics = {
+        'rmse':[], 
+        'mae':[],
+        'r2':[],
+        'loss':[]       
+               }
+    valid_model_metrics = {
         'rmse':[], 
         'mae':[],
         'r2':[],
         'loss':[]     
                }
+    valid_autoencoder_metrics = {
+        'rmse':[], 
+        'mae':[],
+        'r2':[],
+        'loss':[]       
+               }
+    
+    if autoencoder_dict != None:
+        autoencoder_ = True
 
     for epoch in range(num_epochs):
         print(f'EPOCH {epoch + 1}/{num_epochs}:')
 
         # Compute the loss_avg, y_pred and y_true
-        loss_avg, y_true_list, y_pred_list = train_one_epoch(model, optimizer, loss_fn, training_dataloader, complete, train=True)
+        loss_avg, loss_avg_autoencoder, y_true_list, y_pred_list, x_true_list, x_pred_list = train_one_epoch(model, optimizer, loss_fn, training_dataloader, complete, autoencoder_, train=True)
                 
-        train_metrics = calculate_metrics(y_true_list, y_pred_list, train_metrics)
-        train_metrics['loss'].append(loss_avg)
+        train_model_metrics = calculate_metrics(y_true_list, y_pred_list, train_model_metrics)
+        if autoencoder_:
+            train_autoencoder_metrics = calculate_metrics(x_true_list, x_pred_list, train_autoencoder_metrics)
+
+        train_model_metrics['loss'].append(loss_avg)
+        if autoencoder_:
+            train_autoencoder_metrics['loss'].append(loss_avg_autoencoder)
+
         # Validation 
         with torch.no_grad():
-            vloss_avg, vy_true_list, vy_pred_list = train_one_epoch(model, optimizer, loss_fn, validation_dataloader, complete, train=False)
+            vloss_avg, vloss_avg_autoencoder, vy_true_list, vy_pred_list, vx_true_list, vx_pred_list = train_one_epoch(model, optimizer, loss_fn, validation_dataloader, complete, autoencoder_, train=False)
         
-        valid_metrics = calculate_metrics(vy_true_list, vy_pred_list, valid_metrics)
-        valid_metrics['loss'].append(vloss_avg)
+        valid_model_metrics = calculate_metrics(vy_true_list, vy_pred_list, valid_model_metrics)
+        if autoencoder_:
+            valid_autoencoder_metrics = calculate_metrics(vx_true_list, vx_pred_list, valid_autoencoder_metrics)
+
+        valid_model_metrics['loss'].append(vloss_avg)
+        if autoencoder_:
+            valid_autoencoder_metrics['loss'].append(vloss_avg_autoencoder)
+
         if vloss_avg < best_vloss:
             best_vloss = vloss_avg
             path = model.save()
@@ -58,24 +85,42 @@ def train(num_epochs, loss_fn, model, optimizer, scheduler, training_dataloader,
         else:
             scheduler.step() # Update the learning rate as lr^gamma (exponential decay)
         
-        print("train: LOSS %.12f MAE X1:%.4f, Y1:%.4f, Z1:%.4f R2 X1:%.4f, Y1:%.4f, Z1:%.4f RMSE X1:%.6f, Y1:%.6f, Z1:%.6f"
-              % (loss_avg, train_metrics['mae'][epoch][0], train_metrics['mae'][epoch][1], train_metrics['mae'][epoch][2]
-                 ,train_metrics['r2'][epoch][0],train_metrics['r2'][epoch][1],train_metrics['r2'][epoch][2]
-                 ,train_metrics['rmse'][epoch][0],train_metrics['rmse'][epoch][1],train_metrics['rmse'][epoch][2]))
-        print("valid: LOSS %.12f MAE X1:%.4f, Y1:%.4f, Z1:%.4f R2 X1:%.4f, Y1:%.4f, Z1:%.4f RMSE X1:%.6f, Y1:%.6f, Z1:%.6f" 
-              % (vloss_avg, valid_metrics['mae'][epoch][0], valid_metrics['mae'][epoch][1], valid_metrics['mae'][epoch][2]
-                 ,valid_metrics['r2'][epoch][0],valid_metrics['r2'][epoch][1],valid_metrics['r2'][epoch][2]
-                 ,valid_metrics['rmse'][epoch][0],valid_metrics['rmse'][epoch][1],valid_metrics['rmse'][epoch][2]))
-    
+        print("train MODEL: LOSS %.12f MAE X1:%.4f, Y1:%.4f, Z1:%.4f R2 X1:%.4f, Y1:%.4f, Z1:%.4f RMSE X1:%.6f, Y1:%.6f, Z1:%.6f"
+              % (loss_avg, train_model_metrics['mae'][epoch][0], train_model_metrics['mae'][epoch][1], train_model_metrics['mae'][epoch][2]
+                 ,train_model_metrics['r2'][epoch][0],train_model_metrics['r2'][epoch][1],train_model_metrics['r2'][epoch][2]
+                 ,train_model_metrics['rmse'][epoch][0],train_model_metrics['rmse'][epoch][1],train_model_metrics['rmse'][epoch][2]))
+        if autoencoder_:
+            print("train AUTOENCODER: LOSS %.12f MAE X1:%.4f, Y1:%.4f, Z1:%.4f R2 X1:%.4f, Y1:%.4f, Z1:%.4f RMSE X1:%.6f, Y1:%.6f, Z1:%.6f"
+                % (loss_avg_autoencoder, train_autoencoder_metrics['mae'][epoch][0], train_autoencoder_metrics['mae'][epoch][1], train_autoencoder_metrics['mae'][epoch][2]
+                    ,train_autoencoder_metrics['r2'][epoch][0],train_autoencoder_metrics['r2'][epoch][1],train_autoencoder_metrics['r2'][epoch][2]
+                    ,train_autoencoder_metrics['rmse'][epoch][0],train_autoencoder_metrics['rmse'][epoch][1],train_autoencoder_metrics['rmse'][epoch][2]))
+        print("valid MODEL: LOSS %.12f MAE X1:%.4f, Y1:%.4f, Z1:%.4f R2 X1:%.4f, Y1:%.4f, Z1:%.4f RMSE X1:%.6f, Y1:%.6f, Z1:%.6f" 
+              % (vloss_avg, valid_model_metrics['mae'][epoch][0], valid_model_metrics['mae'][epoch][1], valid_model_metrics['mae'][epoch][2]
+                 ,valid_model_metrics['r2'][epoch][0],valid_model_metrics['r2'][epoch][1],valid_model_metrics['r2'][epoch][2]
+                 ,valid_model_metrics['rmse'][epoch][0],valid_model_metrics['rmse'][epoch][1],valid_model_metrics['rmse'][epoch][2]))
+        if autoencoder_:
+            print("valid AUTOENCODER: LOSS %.12f MAE X1:%.4f, Y1:%.4f, Z1:%.4f R2 X1:%.4f, Y1:%.4f, Z1:%.4f RMSE X1:%.6f, Y1:%.6f, Z1:%.6f" 
+                % (vloss_avg_autoencoder, valid_autoencoder_metrics['mae'][epoch][0], valid_autoencoder_metrics['mae'][epoch][1], valid_autoencoder_metrics['mae'][epoch][2]
+                    ,valid_autoencoder_metrics['r2'][epoch][0],valid_autoencoder_metrics['r2'][epoch][1],valid_autoencoder_metrics['r2'][epoch][2]
+                    ,valid_autoencoder_metrics['rmse'][epoch][0],valid_autoencoder_metrics['rmse'][epoch][1],valid_autoencoder_metrics['rmse'][epoch][2]))
+        
     # Save in JSON files the hyperparameter list, the structure of the ensemble model (if you are using it), the metrics of train and validation
     with open(path+'/hyperparam.json', 'w') as f:
         json.dump(hyperparams, f)
     if complete==True:
         with open(path+'/ensemble.json', 'w') as f:
             json.dump(model_dict, f)
-    with open(path+'/train_metrics.json', 'w') as f:
-        json.dump(train_metrics, f)
-    with open(path+'/valid_metrics.json', 'w') as f:
-        json.dump(valid_metrics, f)
+    if autoencoder_:
+        with open(path+'/autoencoder.json', 'w') as f:
+            json.dump(autoencoder_dict, f)
+    with open(path+'/train_model_metrics.json', 'w') as f:
+        json.dump(train_model_metrics, f)
+    with open(path+'/valid_model_metrics.json', 'w') as f:
+        json.dump(valid_model_metrics, f)
+    if autoencoder_:
+        with open(path+'/train_autoencoder_metrics.json', 'w') as f:
+            json.dump(train_autoencoder_metrics, f)
+        with open(path+'/valid_autoencoder_metrics.json', 'w') as f:
+            json.dump(valid_autoencoder_metrics, f)
 
 
--- training/train_one_epoch.py
+++ training/train_one_epoch.py
@@ -2,13 +2,15 @@ import numpy as np
 import gc
 import torch
 
-def train_one_epoch(model, optimizer, loss_fn, dataloader, complete, train):
+def train_one_epoch(model, optimizer, loss_fn, dataloader, complete, autoencoder, train):
 
     # Initialize the lists of predictions and true values
     y_pred_list = []
     y_true_list = []
 
-    loss_epoch = 0 # Initialize the loss
+    loss_epoch = 0 # Initialize the loss for the model
+    loss_epoch_autoencoder = 0 # Initialize the loss of the autoencoder
+
     if train:
         model.train() # Set the model in Training mode
     else:
@@ -30,7 +32,10 @@ def train_one_epoch(model, optimizer, loss_fn, dataloader, complete, train):
         # Make predictions
         if complete:
             y_true = y_true.detach()
-            y_pred, y_pred_models = model(X, y_true)
+            if autoencoder:
+                y_pred, y_pred_models, x_pred = model(X, y_true) # In this case the model return also the x_pred for the autoencoder update! 
+            else:
+                y_pred, y_pred_models = model(X, y_true)
         else:
             y_pred = model(X)
         #print(y_pred)
@@ -38,6 +43,8 @@ def train_one_epoch(model, optimizer, loss_fn, dataloader, complete, train):
         # Compute the loss
         if complete:
             loss = [loss_fn(y_pred, y_true[:,1,:].unsqueeze(1)) for y_pred in y_pred_models]
+            if autoencoder:
+                loss.append(loss_fn(x_pred, X)) # Loss computation of Autoencoder compare X and x_pred because the autoencoder reconstruct the input!
         else:
             loss = loss_fn(y_pred, y_true[:,1,:].unsqueeze(1))
 
@@ -59,13 +66,20 @@ def train_one_epoch(model, optimizer, loss_fn, dataloader, complete, train):
         
         # Accumulate the loss in this epoch
         if complete:
-            loss_epoch += torch.mean(torch.tensor(loss)).detach().item()
+            if autoencoder:
+                loss_epoch += torch.mean(torch.tensor(loss[:-1])).detach().item()
+                loss_epoch_autoencoder += torch.tensor(loss[-1]).detach().item()
+            else:
+                loss_epoch += torch.mean(torch.tensor(loss)).detach().item()
         else:
             loss_epoch += loss.detach().item()
 
         # Add predictions and true values to the lists
         y_true_list += y_true[:,1,:].cpu().tolist()
         y_pred_list += y_pred.squeeze(1).cpu().tolist()
+        if autoencoder:
+            x_true_list += X.cpu().tolist()
+            x_pred_list += x_pred.cpu().tolist()
 
         del X, y_true, y_pred, loss
         gc.collect()
@@ -75,5 +89,6 @@ def train_one_epoch(model, optimizer, loss_fn, dataloader, complete, train):
         
     # Compute the average loss
     loss_avg = loss_epoch / len(dataloader)
+    loss_avg_autoencoder = loss_avg_autoencoder / len(dataloader)
 
-    return loss_avg, y_true_list, y_pred_list
+    return loss_avg, loss_avg_autoencoder, y_true_list, y_pred_list, x_true_list, x_pred_list 
